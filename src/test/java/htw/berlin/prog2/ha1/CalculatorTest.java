1d7b05d27ba59f3d2e35cb27af515e080fd4d1fd
adilyalim <yalimadil26@gmail.com>
2025-10-26 15:40:58 +0100

    1. Commit für neuer grüner Test

--- src/test/java/htw/berlin/prog2/ha1/CalculatorTest.java
+++ src/test/java/htw/berlin/prog2/ha1/CalculatorTest.java
@@ -92,3 +92,19 @@ class CalculatorTest {
     //TODO hier weitere Tests erstellen
 }
 
+    @Test
+    @DisplayName("should display result after  subtraction of two positive single-digit numbers")
+    void testPositiveSubtraktion(){
+        Calculator calc = new Calculator();
+
+        calc.pressDigitKey(6);
+        calc.pressBinaryOperationKey("-");
+        calc.pressDigitKey(3);
+        calc.pressEqualsKey();
+
+        String expected = "3";
+        String actual = calc.readScreen();
+
+        assertEquals(expected,actual);
+    }
+
