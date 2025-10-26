4ab5dbd74ed9bee329af682718bca6a00fb93fe1
adilyalim <yalimadil26@gmail.com>
2025-10-26 16:11:56 +0100

    2. Commit für neuen roten Test

--- src/test/java/htw/berlin/prog2/ha1/CalculatorTest.java
+++ src/test/java/htw/berlin/prog2/ha1/CalculatorTest.java
@@ -90,7 +90,6 @@ class CalculatorTest {
 
 
     //TODO hier weitere Tests erstellen
-}
 
     @Test
     @DisplayName("should display result after  subtraction of two positive single-digit numbers")
@@ -106,5 +105,63 @@ class CalculatorTest {
         String actual = calc.readScreen();
 
         assertEquals(expected,actual);
+    }
+    @Test
+    void  testNegativeTaste() {
+        Calculator calc = new Calculator();
+
+        calc.pressDigitKey(0);
+        calc.pressNegativeKey();
+
+        String exp = "0";
+        String actual = calc.readScreen();
+
+        assertEquals(exp,actual);
+    }
+
+    @Test
+    @DisplayName("should display the latest value and operation after pressing clear key 1 time ")
+    void testClearKeyOnce() {
+
+        Calculator calc  = new Calculator();
+
+        calc.pressDigitKey(8);
+        calc.pressBinaryOperationKey("+");
+        calc.pressDigitKey(2);
+        calc.pressEqualsKey();
+
+
+        calc.pressClearKey();
+
+        int expectedLatestValue = 10;
+        String expectedLatesOp = "+";
+
+        assertEquals(expectedLatestValue,calc.getLatestValue());
+        assertEquals(expectedLatesOp,calc.getLatestOperation());
+
+    }
+    @Test
+    @DisplayName("should display the latest value as 0 and latest operation as empty after pressing clear key 2 times ")
+    void testClearKeyTwice() {
+
+        Calculator calc  = new Calculator();
+
+        calc.pressDigitKey(9);
+        calc.pressBinaryOperationKey("-");
+        calc.pressDigitKey(3);
+        calc.pressEqualsKey();
+
+        String expected = "6";
+        String actual = calc.readScreen();
+        assertEquals(expected,actual);
+
+        calc.pressClearKey();
+        calc.pressClearKey();
+        expected = "0";
+        double expLatVal = 0.0;
+        String expLatOp = "";
+
+        assertEquals(expLatVal,calc.getLatestValue());
+        assertEquals(expLatOp,calc.getLatestOperation());
     }
 
